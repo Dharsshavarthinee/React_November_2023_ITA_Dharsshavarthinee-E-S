17723a020d4575b162f329ec1c3687511feca22e
Dharsshavarthinee <124855867+Dharsshavarthinee@users.noreply.github.com>
2023-11-17 10:45:29 +0530

    DAY-4

---
+++ day4/reactapp1src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ListItems from './components/ListItems';
+import EmployeeDetails from './components/EmployeeDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <ListItems/>
+      <EmployeeDetails/>
+    </div>
+  );
+}
+
+export default App;
